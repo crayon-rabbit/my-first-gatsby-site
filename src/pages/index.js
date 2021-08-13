5d614479b0ab18094812711831d547c25cd1fa23
chenhao <chenhao@createw.com>
2021-08-13 15:16:20 +0800

    Finished Gatsby tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-// Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 
-// Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="首页标题">
@@ -13,5 +11,4 @@ const IndexPage = () => {
   );
 };
 
-// Step 3: Export your component
 export default IndexPage;
