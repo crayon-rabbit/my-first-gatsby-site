392ade0bf6e9abc7ff047a459b83150d1512483f
chenhao <chenhao@createw.com>
2021-08-13 13:47:32 +0800

    Finish Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 // Step 1: Import React
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="首页">
-      <p>首</p>
+    <Layout pageTitle="首页标题">
+      <p>首页</p>
+      <StaticImage src="../images/600.jpg" alt="A kitten" />
     </Layout>
   );
 };
